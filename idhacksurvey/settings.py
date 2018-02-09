d8adf0926d707189ac69dd0f039461bff35acb78
Abdisalan <abdisalan@acenna.com>
2018-02-09 16:19:21 -0500

    Turned Debug off

--- idhacksurvey/settings.py
+++ idhacksurvey/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.getenv('SECRET_KEY', 'add_your_real_secret_key_here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
