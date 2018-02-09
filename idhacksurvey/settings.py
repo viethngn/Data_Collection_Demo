5cdae4ea6a78cffc3e14efce10a49e8b7e0f06db
Abdisalan <abdisalan@acenna.com>
2018-02-09 14:55:36 -0500

    removed secret

--- idhacksurvey/settings.py
+++ idhacksurvey/settings.py
@@ -21,7 +21,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "=ktk-)87745t9teo8%%5gd&a^7uvzf*glafy=ctdw6b8t9+fzo"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
